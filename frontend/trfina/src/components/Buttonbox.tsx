06aedbd6b3cb6e038c9f12e91199db39744a8e76
ansari-na <nadeemansaria7866@gmail.comgit config --listgit config --global user.email nadeemansaria7866@gmail.com>
2026-05-23 11:03:08 +0530

    styling

--- frontend/trfina/src/components/Buttonbox.tsx
+++ frontend/trfina/src/components/Buttonbox.tsx
@@ -6,7 +6,7 @@ interface buttonboxtype{
 
 export function Buttonbox({label,onclick}:buttonboxtype) {
     return <div className="ml-2  ">
-        <button onClick={onclick} className="bg-black rounded-md w-86 text-white text-center pb-1.5 pt-1  mt-3 shadow-md cursor-pointer font-bold">{label}</button>
+        <button onClick={onclick} className="bg-black rounded-md w-full  text-white text-center pb-1.5 pt-1  mt-3 shadow-md cursor-pointer font-bold">{label}</button>
     </div>
 }
 
--- frontend/trfina/src/components/Subheading.tsx
+++ frontend/trfina/src/components/Subheading.tsx
@@ -1,6 +1,6 @@
 
 export function Subheading({label}:{label:string}){
-    return <div className="text-gray-500 w-88 text-center">
+    return <div className="text-gray-500 w-full text-center">
         <div>{label}</div>
     </div>
 }
